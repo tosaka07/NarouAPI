01238f8417061d153c654ee6c45c43ab467b0c90
tosaka07 <tosaka.tech@gmail.com>
2019-12-31 21:03:01 +0900

    Initial Commit

---
+++ NarouAPI/NarouAPI.h
@@ -0,0 +1,19 @@
+//
+//  NarouAPI.h
+//  NarouAPI
+//
+//  Created by 坂上 翔悟 on 2019/12/31.
+//  Copyright © 2019 tosaka.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NarouAPI.
+FOUNDATION_EXPORT double NarouAPIVersionNumber;
+
+//! Project version string for NarouAPI.
+FOUNDATION_EXPORT const unsigned char NarouAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NarouAPI/PublicHeader.h>
+
+
